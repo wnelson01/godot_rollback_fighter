b9801d09cd586d8de6cba037e41404d8edd3a3c2
David Snopek <dsnopek@gmail.com>
2022-01-26 11:02:05 -0600

    Implement "match flow" by setting up SyncManager and responding to signals

--- Main.gd
+++ Main.gd
@@ -4,11 +4,17 @@ onready var connection_panel = $CanvasLayer/ConnectionPanel
 onready var host_field = $CanvasLayer/ConnectionPanel/GridContainer/HostField
 onready var port_field = $CanvasLayer/ConnectionPanel/GridContainer/PortField
 onready var message_label = $CanvasLayer/MessageLabel
+onready var sync_lost_label = $CanvasLayer/SyncLostLabel
 
 func _ready() -> void:
 	get_tree().connect("network_peer_connected", self, "_on_network_peer_connected")
 	get_tree().connect("network_peer_disconnected", self, "_on_network_peer_disconnected")
 	get_tree().connect("server_disconnected", self, "_on_server_disconnected")
+	SyncManager.connect("sync_started", self, "_on_SyncManager_sync_started")
+	SyncManager.connect("sync_stopped", self, "_on_SyncManager_sync_stopped")
+	SyncManager.connect("sync_lost", self, "_on_SyncManager_sync_lost")
+	SyncManager.connect("sync_regained", self, "_on_SyncManager_sync_regained")
+	SyncManager.connect("sync_error", self, "_on_SyncManager_sync_error")
 
 func _on_ServerButton_pressed() -> void:
 	var peer = NetworkedMultiplayerENet.new()
@@ -26,9 +32,46 @@ func _on_ClientButton_pressed() -> void:
 
 func _on_network_peer_connected(peer_id: int):
 	message_label.text = "Connected!"
+	SyncManager.add_peer(peer_id)
+	
+	if get_tree().is_network_server():
+		message_label.text = "Starting..."
+		# Give a little time to get ping data.
+		yield(get_tree().create_timer(2.0), "timeout")
+		SyncManager.start()
 
 func _on_network_peer_disconnected(peer_id: int):
 	message_label.text = "Disconnected"
+	SyncManager.remove_peer(peer_id)
 
 func _on_server_disconnected() -> void:
 	_on_network_peer_disconnected(1)
+
+func _on_ResetButton_pressed() -> void:
+	SyncManager.stop()
+	SyncManager.clear_peers()
+	var peer = get_tree().network_peer
+	if peer:
+		peer.close_connection()
+	get_tree().reload_current_scene()
+
+func _on_SyncManager_sync_started() -> void:
+	message_label.text = "Started!"
+
+func _on_SyncManager_sync_stopped() -> void:
+	pass
+
+func _on_SyncManager_sync_lost() -> void:
+	sync_lost_label.visible = true
+
+func _on_SyncManager_sync_regained() -> void:
+	sync_lost_label.visible = false
+
+func _on_SyncManager_sync_error(msg: String) -> void:
+	message_label.text = "Fatal sync error: " + msg
+	sync_lost_label.visible = false
+	
+	var peer = get_tree().network_peer
+	if peer:
+		peer.close_connection()
+	SyncManager.clear_peers()
