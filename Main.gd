0c52406383c5c6eaf3f7b2495fc7f1357026d537
David Snopek <dsnopek@gmail.com>
2022-01-26 11:24:31 -0600

    Add the players

--- Main.gd
+++ Main.gd
@@ -34,6 +34,12 @@ func _on_network_peer_connected(peer_id: int):
 	message_label.text = "Connected!"
 	SyncManager.add_peer(peer_id)
 	
+	$ServerPlayer.set_network_master(1)
+	if get_tree().is_network_server():
+		$ClientPlayer.set_network_master(peer_id)
+	else:
+		$ClientPlayer.set_network_master(get_tree().get_network_unique_id())
+	
 	if get_tree().is_network_server():
 		message_label.text = "Starting..."
 		# Give a little time to get ping data.
