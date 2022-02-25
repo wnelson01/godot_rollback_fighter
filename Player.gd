a55e4a830c60ff9fcfee91da20cd38b3cbaa0583
David Snopek <dsnopek@gmail.com>
2022-02-25 14:11:25 -0600

    Create a state mismatch bug

--- Player.gd
+++ Player.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 const Bomb = preload("res://Bomb.tscn")
 
+var speed := 0.0
+
 func _get_local_input() -> Dictionary:
 	var input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
@@ -19,7 +21,13 @@ func _predict_remote_input(previous_input: Dictionary, ticks_since_real_input: i
 	return input
 
 func _network_process(input: Dictionary) -> void:
-	position += input.get("input_vector", Vector2.ZERO) * 8
+	var input_vector = input.get("input_vector", Vector2.ZERO)
+	if input_vector != Vector2.ZERO:
+		if speed < 8.0:
+			speed += 0.2
+		position += input_vector * speed
+	else:
+		speed = 0.0
 	
 	if input.get("drop_bomb", false):
 		SyncManager.spawn("Bomb", get_parent(), Bomb, { position = global_position })
