4d20f59b5eac8cf1933bd5fb5d990eea17d5196a
David Snopek <dsnopek@gmail.com>
2022-04-26 15:13:02 -0500

    Animate the bombs and explosions using NetworkAnimationPlayer

--- Bomb.gd
+++ Bomb.gd
@@ -3,10 +3,12 @@ extends Node2D
 const Explosion = preload("res://Explosion.tscn")
 
 onready var explosion_timer = $ExplosionTimer
+onready var animation_player = $NetworkAnimationPlayer
 
 func _network_spawn(data: Dictionary) -> void:
 	global_position = data['position']
 	explosion_timer.start()
+	animation_player.play("Tick")
 
 func _on_ExplosionTimer_timeout() -> void:
 	SyncManager.spawn("Explosion", get_parent(), Explosion, {
--- Explosion.gd
+++ Explosion.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
 onready var despawn_timer = $DespawnTimer
+onready var animation_player = $NetworkAnimationPlayer
 
 func _network_spawn(data: Dictionary) -> void:
 	global_position = data['position']
 	despawn_timer.start()
+	animation_player.play("Explode")
 
 func _on_DespawnTimer_timeout() -> void:
 	SyncManager.despawn(self)
